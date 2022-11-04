970ece5f79ad73d7a841740be464eb82f8f67397
bilalehsan <bilal.ehsan@yahoo.com>
2022-11-04 18:01:34 +0500

    Web RTC implemented device to device only call enable

--- app/src/main/java/com/example/afjtracking/utils/Constants.kt
+++ app/src/main/java/com/example/afjtracking/utils/Constants.kt
@@ -4,17 +4,17 @@ import java.util.concurrent.TimeUnit
 
 object Constants {
 
-    // const val BASE_URL ="http://vmi808920.contaboserver.net/api/"
-    // const val FAILED_API_TAG = "vmi808920.contaboserver.net"
+     const val BASE_URL ="http://vmi808920.contaboserver.net/api/"
+     const val FAILED_API_TAG = "vmi808920.contaboserver.net"
 
-    const val BASE_URL = "http://afjdev.hnhtechpk.com/api/"
-    const val FAILED_API_TAG = "afjdev.hnhtechpk.com"
+    //const val BASE_URL = "http://afjdev.hnhtechpk.com/api/"
+    //const val FAILED_API_TAG = "afjdev.hnhtechpk.com"
 
     //  const val BASE_URL = "http://192.168.18.69:8000/api/"
     //  const val FAILED_API_TAG = "192.168.18.69:8000"
 
 
-    val WEBSOCKET_URL = "ws://192.168.18.69:6001/mobile?token="
+    val WEBSOCKET_URL = "ws://vmi808920.contaboserver.net:6001/mobile?token="
 
     var isCallEnded: Boolean = false
     var isIntiatedNow: Boolean = true
